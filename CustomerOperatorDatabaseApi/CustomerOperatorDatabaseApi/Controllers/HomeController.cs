8e33b7d3c478fbf7cdb0d2f3d0a1c70a4de19792
Natan <155454141+NatanProtector@users.noreply.github.com>
2026-03-20 22:21:03 +0200

    Home controller and Views

---
+++ CustomerOperatorDatabaseApi/CustomerOperatorDatabaseApi/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerOperatorDatabaseApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
